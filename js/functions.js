8e3ed79bf8c585e76b4bfaca22375edccbdac696
VictoriaKhr <18sweetcandy@mail.ru>
2025-12-08 11:47:25 +0300

    задание 2.2

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,7 @@
-// Функция для проверки длины строки
 function checkStringLength(str, maxLength) {
   return str.length <= maxLength;
 }
 
-// Функция для проверки палиндрома
 function isPalindrome(str) {
   const normalizedStr = str.toLowerCase().replaceAll(' ', '');
 
